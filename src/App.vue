2f0d4787e720ab51dd5b44c1badc9acc75039202
Wilian Cesar <wilcesar@live.com>
2025-08-19 18:16:35 -0300

    Criando a lista de ingredientes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script lang="ts">
   import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+import SelecionarIngredientes from './components/SelecionarIngredientes.vue';
   
   export default{
-    components: {Banner}
+    components: {Banner, ConteudoPrincipal,SelecionarIngredientes}
   }
 </script>
 <template>
   <Banner />
-
+  <ConteudoPrincipal />
+  <SelecionarIngredientes/>
 </template>
 <style scoped>
 
